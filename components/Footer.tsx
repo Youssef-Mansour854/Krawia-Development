0bb8290a18a8727379fdfa77c3c48a5d009b887a
Youssef-Mansour <114363242+Youssef-Mansour854@users.noreply.github.com>
2026-08-10 21:55:57 +0300

    fix(address): update headquarters address text to start directly from Damanhour

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
                 <svg className="w-4 h-4 text-amber-400 shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
-                <span>جمهورية مصر العربية - دمنهور (شارع الضغط العالي - محافظة البحيرة)</span>
+                <span>دمنهور - شارع الضغط العالي - محافظة البحيرة</span>
               </p>
 
               <p className="flex items-center gap-2">
